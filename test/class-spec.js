c31af02c25a68f5e0006d85b0c19d3f45172f561
sybil <jordan.sybil@hotmail.com>
2024-03-13 22:10:23 -0700

    done

--- test/class-spec.js
+++ test/class-spec.js
@@ -2,35 +2,44 @@ const chai = require("chai");
 const expect = chai.expect;
 
 const { Word } = require("../class");
-let word;
+
 describe("Word", function () {
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect('word').to.have.property()
-
+   const newWord = new Word(' Pringles')
+      expect(newWord).to.have.property('word')
 
     });
 
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      const newWord = new Word('Pringles')
+      expect(newWord.word).to.equal('Pringles')
+
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      const newWord = new Word('Pringles')
+      const result = newWord.removeVowels()
+      expect(result).to.equal('Prngls')
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+
+      const newWord = new Word('dog')
+      const result = newWord.removeConsonants()
+      expect(result).to.equal('o')
     });
   });
 
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      const newWord = new Word('banana')
+      const result = newWord.pigLatin()
+      expect(result).to.equal('ananabay')
     });
   });
 });
